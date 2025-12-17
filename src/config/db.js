f7f49f66c12f0d9113c886f06269950f9d507530
Suriyaprakash <suriyaprakashrm25@gmail.com>
2025-12-17 14:49:12 +0530

    database connection updated

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017");
+    await mongoose.connect(process.env.MONGO_URI);
     // await mongoose.connect("mongodb+srv://vlajusha_db_user:ajusha@123@cluster0.n9xrjy5.mongodb.net/?appName=Cluster0");
 
     console.log("MongoDB connected successfully");
